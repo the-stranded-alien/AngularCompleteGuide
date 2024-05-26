81fa372abdd6b35dd88b66c9c15a18ff50dcbdb8
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2024-05-26 23:59:56 +0530

    Angular Essentials

---
+++ 01_Angular_Essentials/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import {UserComponent} from "./user/user.component";
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    HeaderComponent,
+    UserComponent
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {}
