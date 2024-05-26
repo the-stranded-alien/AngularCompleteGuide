458de15e2f07cacc8af70254726ed382b8f211d4
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2024-05-27 00:39:45 +0530

    Angular Essentials

--- 01_Angular_Essentials/src/app/app.component.ts
+++ 01_Angular_Essentials/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import {UserComponent} from "./user/user.component";
+import { DUMMY_USERS } from "./dummy-users";
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,10 @@ import {UserComponent} from "./user/user.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id: ' + id);
+  }
+}
--- 01_Angular_Essentials/src/app/user/user.component.ts
+++ 01_Angular_Essentials/src/app/user/user.component.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import {Component, computed, signal, Input, input, Output, EventEmitter } from '@angular/core';
 import { DUMMY_USERS } from "../dummy-users";
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -11,17 +11,37 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  // Input using Signals
+  // avatar = input<string>()
+  // name = input<string>()
+  // avatar = input.required();
+  // name = input.required();
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+
+  // imagePath = computed(() => {
+  //   return 'assets/users/' + this.avatar();
+  // });
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
   // selectedUser = DUMMY_USERS[randomIndex];
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   // get imagePath() {
   //   return 'assets/users/' + this.selectedUser.avatar;
   // }
 
   onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+    // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    // this.selectedUser.set(DUMMY_USERS[randomIndex]);
     // this.selectedUser = DUMMY_USERS[randomIndex];
+    this.select.emit(this.id);
   }
 }
