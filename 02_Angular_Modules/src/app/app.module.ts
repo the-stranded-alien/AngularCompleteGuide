288db89d37e016038b6b093fae771fb702bc65e1
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2024-05-29 22:55:58 +0530

    Angular Modules

--- 02_Angular_Modules/src/app/app.module.ts
+++ 02_Angular_Modules/src/app/app.module.ts
@@ -1,24 +1,32 @@
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+// import { FormsModule } from "@angular/forms";
+
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { BrowserModule } from "@angular/platform-browser";
+// import { CardComponent } from "./shared/card/card.component";
+// import { TasksComponent } from "./tasks/tasks.component";
+// import { TaskComponent } from "./tasks/task/task.component";
+// import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 
 // Declarations: For Non-StandAlone Components
 // Imports: For StandAlone Components and Also for Other Modules
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent, HeaderComponent, UserComponent
   ],
   bootstrap: [AppComponent],
   imports: [
     BrowserModule,
-    HeaderComponent,
-    UserComponent,
-    TasksComponent
+    // FormsModule,
+    SharedModule,
+    TasksModule
+    // HeaderComponent,
+    // UserComponent,
+    // TasksComponent
   ],
 })
-export class AppModule {
-
-}
+export class AppModule {}
